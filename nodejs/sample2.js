5cb9f7090fba1ca52f00dc1ed584b608c92aaf74
Kaoru Chisen <cordx56@cordx.net>
2020-08-14 02:34:42 +0900

    add new sample2

--- nodejs/sample2.js
+++ nodejs/sample2.js
@@ -7,7 +7,7 @@ HTMLHEAD = `
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title>Guess Number</title>
+        <title>Access counter</title>
         <meta content="text/html; charset=utf-8" http-equiv="content-type" />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
     </head>
@@ -15,8 +15,7 @@ HTMLHEAD = `
         <form action="" method="post">
 `
 HTMLFOOT = `
-            <p>Input Your Guess: <input type="text" name="num" /></p>
-            <p><input type="submit" /></p>
+            <p><input type="submit" value="reset" /></p>
         </form>
     </body>
 `
@@ -28,40 +27,18 @@ app.use(session({
     saveUninitialized: true
 }));
 
-app.get("/numguess", (req, res) => {
-    res.status(200).send(HTMLHEAD + HTMLFOOT);
+app.get("/", (req, res) => {
+    if (req.session.count === undefined) {
+        req.session.count = 0;
+    } else {
+        req.session.count++;
+    }
+    res.status(200).send(HTMLHEAD + "<p>" + String(req.session.count) + "</p>" + HTMLFOOT);
 });
 
-app.post("/numguess", (req, res) => {
-    if (req.session.rnum === undefined) {
-        req.session.rnum = Math.floor(Math.random() * 100) + 1;
-        req.session.count = 0;
-    }
-    out = "";
-    if (req.body !== undefined && req.body.num !== undefined) {
-        num = parseInt(req.body.num);
-        rnum = parseInt(req.session.rnum);
-        count = parseInt(req.session.count);
-        count += 1;
-        req.session.count = count;
-        out += "<p>";
-        if (num !== NaN && rnum !== NaN) {
-            out += count + "tries, " + num + ": ";
-            if (num < rnum) {
-                out += "small!";
-            } else if (num > rnum) {
-                out += "big!";
-            } else if (num == rnum) {
-                out += "Congratulations!";
-                delete req.session.rnum;
-                delete req.session.count;
-            }
-        } else {
-            out += "error";
-        }
-        out += "</p>";
-    }
-    res.status(200).send(HTMLHEAD + out + HTMLFOOT);
+app.post("/", (req, res) => {
+    req.session.count = 0;
+    res.status(200).send(HTMLHEAD + "<p>" + String(req.session.count) + "</p>" + HTMLFOOT);
 });
 
 app.listen(8000);
--- php/sample2.php
+++ php/sample2.php
@@ -1,42 +1,24 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title>Guess Number</title>
+        <title>Access counter</title>
         <meta content="text/html; charset=utf-8" http-equiv="content-type" />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
     </head>
     <body>
-	    <form action="" method="post">
+        <form action="" method="post">
             <?php
             session_start();
-            if (!isset($_SESSION["rnum"])) {
-                $_SESSION["rnum"] = rand(1, 100);
+            if (!isset($_SESSION["count"]) || $_SERVER["REQUEST_METHOD"] === "POST") {
                 $_SESSION["count"] = 0;
+            } else {
+                $_SESSION["count"]++;
             }
-            if (isset($_POST["num"])) {
-                print("<p>");
-                $num = $_POST["num"];
-                $rnum = $_SESSION["rnum"];
-                $count = $_SESSION["count"];
-                if (is_numeric($num) && is_numeric($rnum) && is_numeric($count)) {
-                    $count += 1;
-                    $_SESSION["count"] = $count;
-                    print($count."tries, ".$num.": ");
-                    if ($num < $rnum) {
-                        print("small!");
-                    } else if ($num > $rnum) {
-                        print("big!");
-                    } else {
-                        print("Congratulations!");
-                        unset($_SESSION["rnum"]);
-                        unset($_SESSION["count"]);
-                    }
-                } else print("error");
-                print("</p>");
-            }
+            print("<p>");
+            print($_SESSION["count"]);
+            print("</p>");
             ?>
-            <p>Input Your Guess: <input type="text" name="num" /></p>
-            <p><input type="submit" /></p>
+            <p><input type="submit" value="reset" /></p>
         </form>
     </body>
 </html>
