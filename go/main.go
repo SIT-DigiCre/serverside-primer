0d09b919811243f7e67ef9935537a9a6004c8a3b
Kaoru Chisen <cordx56@cordx.net>
2020-08-23 04:16:16 +0900

    add main.go

---
+++ go/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+
+	store := cookie.NewStore([]byte("がっこうぐらし！"))
+	router.Use(sessions.Sessions("mysession", store))
+
+	router.GET("/", welcome)
+	router.GET("/countup", countup)
+	router.POST("/countup", countupReset)
+
+	router.Run(":8000")
+}
