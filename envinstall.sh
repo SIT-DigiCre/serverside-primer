ff0a2c8146bb4adc8e8f02e479d332cd62125d01
Kaoru Chisen <cordx56@cordx.net>
2020-09-18 12:57:04 +0900

    split installscript.sh

--- envinstall.sh
+++ envinstall.sh
@@ -3,17 +3,6 @@ sudo apt update
 sudo apt install -y vim emacs curl ssh
 sudo apt install -y php python3 python3-pip nodejs npm golang
 
-# install Python packages
-sudo pip3 install pipenv flask
-cd ~/serverside-primer/python
-sudo pipenv install --system
-cd ~/serverside-primer
-
-# install Nodejs packages
-cd ~/serverside-primer/nodejs
-npm install
-cd ~/serverside-primer
-
 # install VSCode
 if [ -n "$GUI" ]; then
     curl -L -o code.deb "https://go.microsoft.com/fwlink/?LinkID=760868"
---
+++ initscript.sh
@@ -0,0 +1,16 @@
+sudo apt update
+sudo apt install -y git
+git clone https://github.com/SIT-DigiCre/serverside-primer.git ~/serverside-primer
+cd ~/serverside-primer
+./envinstall.sh
+
+# install Python packages
+sudo pip3 install pipenv flask
+cd ~/serverside-primer/python
+sudo pipenv install --system
+cd ~/serverside-primer
+
+# install Nodejs packages
+cd ~/serverside-primer/nodejs
+npm install
+cd ~/serverside-primer
